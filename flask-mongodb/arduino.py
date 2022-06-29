d1d8b09b8d91f323d6ca74cb02b021509cdcf967
A.Barutchi <ahmad.barutchi@protonmail.com>
2022-06-29 23:15:40 +0200

    all values been added

--- flask-mongodb/arduino.py
+++ flask-mongodb/arduino.py
@@ -1,7 +1,6 @@
-from flask import Flask
-from pymongo import MongoClient
 import serial
 import time
+from pymongo import MongoClient
 
 
 device = 'COM3'  # this will have to be changed to the serial port you are using
@@ -49,49 +48,3 @@ try:
             print("An exception occurred ::", e)
 except:
     print("Failed to get data from Arduino!")
-
-app = Flask(__name__)
-
-
-@app.route("/get-temp", methods=['GET'])
-def get_temp():
-    cursor = collection.find()
-    valeur = []
-    for record in cursor:
-        print(record)
-        valeur.append(record["temp"])
-        valeur.append(record["temp2"])
-        valeur.append(record["humidity"])
-        valeur.append(record["pos"])
-        valeur.append(record["humain"])
-        valeur.append(record["fire"])
-    print(valeur)
-    i = 0
-    y = len(valeur)
-    y /= 6
-    y = int(y)
-    y = range(0, y)
-    print("y: ")
-    print(y)
-    presets = {}
-    for x in y:
-        preset = {
-            "temp": valeur[i+0],
-            "temp2": valeur[i+1],
-            "humidity": valeur[i+2],
-            "pos": valeur[i+3],
-            "humain": valeur[i+4],
-            "fire": valeur[i+5]
-        }
-        print("preset")
-        print(preset)
-        i += 6
-        presets[i] = preset
-        print("presets")
-        print(presets)
-    return presets
-
-
-if __name__ == "__main__":
-    print ('Api start ! ')
-    app.run(debug=True)
