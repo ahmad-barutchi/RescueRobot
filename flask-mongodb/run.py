002765a078f6c2b27510c60982d6250533b2238a
A.Barutchi <ahmad.barutchi@protonmail.com>
2022-07-05 16:12:57 +0200

    temps are fetched on Ngx-admin

--- flask-mongodb/run.py
+++ flask-mongodb/run.py
@@ -1,7 +1,9 @@
+import flask
 from flask import Flask
 from pymongo import MongoClient
 import serial
 import time
+from flask_cors import CORS, cross_origin
 
 device = 'COM3'  # this will have to be changed to the serial port you are using
 try:
@@ -11,8 +13,8 @@ except:
     print("Failed to connect on", device)
 
 try:
-    conn = MongoClient("localhost", 16577)
-    print("Connected successfully!!!")
+    conn = MongoClient("localhost", 9517)
+    print("Connected successfully to mongoDB!!!")
 except:
     print("Could not connect to MongoDB")
 
@@ -29,7 +31,6 @@ try:
         data = arduino.readline()  # read the data from the arduino
         data = data[0:-2]
         data = data.decode('ascii')
-        print(data)
         pieces = data.split(" ")  # split the data by the space
         print(pieces)
         frame = {
@@ -49,28 +50,29 @@ except:
     print("Failed to get data from Arduino!")
 
 app = Flask(__name__)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+cors = CORS(app, resources={r"/get-temp": {"origins": "http://localhost:5000"}})
 
 
+@cross_origin(origin='http://localhost:*',headers=['Access-Control-Allow-Origin', 'Access-Control-Allow-Headers', 'Accept', 'Accept-Language', 'X-Authorization'])
 @app.route("/get-temp", methods=['GET'])
 def get_temp():
     cursor = collection.find()
     valeur = []
     for record in cursor:
-        print(record)
         valeur.append(record["temp"])
         valeur.append(record["temp2"])
         valeur.append(record["humidity"])
         valeur.append(record["pos"])
         valeur.append(record["human"])
         valeur.append(record["fire"])
-    print(valeur)
     i = 0
     y = len(valeur)
     y /= 6
     y = int(y)
     y = range(0, y)
-    print("y: ")
-    print(y)
+
     presets = {}
     for x in y:
         preset = {
@@ -81,13 +83,13 @@ def get_temp():
             "human": valeur[i + 4],
             "fire": valeur[i + 5]
         }
-        print("preset")
-        print(preset)
         i += 6
         presets[i-6] = preset
-        print("presets")
-        print(presets)
-    return presets
+    print("presets")
+    print(presets)
+    response = flask.jsonify(presets)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
 
 if __name__ == "__main__":
